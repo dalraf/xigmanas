db4da0ec68e5b16ca6b68dbedd31c6a1863b25d6
Daniel Fonseca Alves <dalraf@gmail.com>
2019-06-03 15:35:00 -0300

    Adicionado permission

---
+++ permission.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+find ${1} -type f  -exec setfacl -m everyone@:full_set::allow {} \;
+find ${1} -type f  -exec setfacl -m group@:full_set::allow {} \;
+find ${1} -type f  -exec setfacl -m owner@:full_set::allow {} \;
+find ${1} -type f -exec setfacl -m group:usuários_do_domínio:full_set::allow {} \;
+find ${1} -type d -exec setfacl -m everyone@:full_set:fd:allow {} \;
+find ${1} -type d -exec setfacl -m group@:full_set:fd:allow {} \;
+find ${1} -type d -exec setfacl -m owner@:full_set:fd:allow {} \;
+find ${1} -type d -exec setfacl -m group:usuários_do_domínio:full_set:fd:allow {} \;
