0199eae447b14422bbbba62fb2b39d827a4b8a9c
Daniel Fonseca Alves <dalraf@gmail.com>
2019-06-03 15:46:59 -0300

    Adciioando bash

--- permission.sh
+++ permission.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/sh
 find ${1} -type f  -exec setfacl -m everyone@:full_set::allow {} \;
 find ${1} -type f  -exec setfacl -m group@:full_set::allow {} \;
 find ${1} -type f  -exec setfacl -m owner@:full_set::allow {} \;
