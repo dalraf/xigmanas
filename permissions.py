473400bb27cd785c78c77708e2bc789bc91eb0a7
Daniel Fonseca Alves <dalraf@gmail.com>
2022-09-12 16:26:05 -0300

    Correcao

--- permissions.py
+++ permissions.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+from pathlib import Path
 
 lista_acl_obj = [
     'user:root',
@@ -8,7 +9,7 @@ lista_acl_obj = [
     'everyone@',
 ]
 
-caminho = sys.argv[1]
+caminho = Path(f'"{sys.argv[1]}"')
 
 for obj in lista_acl_obj:
     comando = f'setfacl -R -m {obj}:rwxpDdaARWcCo-:fd-----:allow {caminho}'
