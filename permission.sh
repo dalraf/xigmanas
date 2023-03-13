5a26591f5ceb25668f1711460f94b5bf8dae2234
Daniel Fonseca Alves <dalraf@gmail.com>
2023-03-13 16:05:12 -0300

    Removido find para a execução ficar mais rapida

--- permission.sh
+++ permission.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-find ${1} -exec setfacl -b {} \;
-find ${1} -exec chown root {} \;
-find ${1} -exec chgrp wheel {} \;
-find ${1} -type f  -exec setfacl -m everyone@:full_set::allow {} \;
-find ${1} -type f  -exec setfacl -m group@:full_set::allow {} \;
-find ${1} -type f  -exec setfacl -m owner@:full_set::allow {} \;
-find ${1} -type f -exec setfacl -m group:usuários_do_domínio:full_set::allow {} \;
-find ${1} -type d -exec setfacl -m everyone@:full_set:fd:allow {} \;
-find ${1} -type d -exec setfacl -m group@:full_set:fd:allow {} \;
-find ${1} -type d -exec setfacl -m owner@:full_set:fd:allow {} \;
-find ${1} -type d -exec setfacl -m group:usuários_do_domínio:full_set:fd:allow {} \;
+setfacl -R -b ${1}
+chown -R root ${1}
+chgrp -R wheel ${1}
+setfacl -R -m everyone@:full_set::allow ${1}
+setfacl -R -m group@:full_set::allow ${1}
+setfacl -R -m owner@:full_set::allow ${1}
+setfacl -R -m group:usuários_do_domínio:full_set::allow ${1}
+
