60c66486a903302d9ea905adb2334ea6c60809a9
Daniel Fonseca Alves <dalraf@gmail.com>
2023-03-16 17:19:32 -0300

    Correção pela ordem de comandos

--- permission-sync.sh
+++ permission-sync.sh
@@ -4,7 +4,8 @@ chown -R root ${1}
 chgrp -R wheel ${1}
 setfacl -R -m owner@:rwxpDdaARWcCo-:fd----I:allow ${1}
 setfacl -R -m group@:rwxpDdaARWcCo-:fd----I:allow ${1}
-setfacl -R -m user:syncthing:rwxpDdaARWcCo-:fdi----:allow ${1}
+setfacl -R -m user:syncthing:rwxpDdaARWcCo-:fd----:allow ${1}
+setfacl -R -m everyone@:rwxpDdaARWcCo-:fdi----:allow ${1}
 setfacl -m everyone@:r-x---a-R-c---:-------:allow ${1}
-setfacl -R -m everyone@:rwxpDdaARWcCo-:fdi----:allow ${1}/
+
 
