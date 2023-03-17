ed0fad9ece744bcf4b7060a814e63a5539e29a25
Daniel Fonseca Alves <dalraf@gmail.com>
2023-03-17 11:51:20 -0300

    Removido inhenance

--- permission-sync.sh
+++ permission-sync.sh
@@ -5,7 +5,7 @@ chgrp -R wheel ${1}
 setfacl -R -m owner@:rwxpDdaARWcCo-:fd----I:allow ${1}
 setfacl -R -m group@:rwxpDdaARWcCo-:fd----I:allow ${1}
 setfacl -R -m user:syncthing:rwxpDdaARWcCo-:fd----:allow ${1}
-setfacl -R -m everyone@:rwxpDdaARWcCo-:fdi----:allow ${1}
-setfacl -m everyone@:r-x---a-R-c---:-------:allow ${1}
+setfacl -R -m everyone@:rwxpDdaARWcCo-:fd----:allow ${1}
+
 
 
--- permission.sh
+++ permission.sh
@@ -2,8 +2,7 @@
 setfacl -R -b ${1}
 chown -R root ${1}
 chgrp -R wheel ${1}
-setfacl -R -m everyone@:full_set::allow ${1}
-setfacl -R -m group@:full_set::allow ${1}
-setfacl -R -m owner@:full_set::allow ${1}
-setfacl -R -m group:usuários_do_domínio:full_set::allow ${1}
+setfacl -R -m group@:full_set:fd:allow ${1}
+setfacl -R -m owner@:full_set:fd:allow ${1}
+setfacl -R -m everyone@:full_set:fd:allow ${1}
 
