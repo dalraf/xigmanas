27ce401ece5b3b7b1dd77e5a39426b1532b036c7
Daniel Fonseca Alves <dalraf@gmail.com>
2024-02-28 17:02:11 -0300

    Correção para nome de diretório

--- permission.sh
+++ permission.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-setfacl -R -b ${1}
-chown -R root ${1}
-chgrp -R wheel ${1}
-setfacl -R -m group@:full_set:fd:allow ${1}
-setfacl -R -m owner@:full_set:fd:allow ${1}
-setfacl -R -m everyone@:full_set:fd:allow ${1}
+setfacl -R -b "${1}"
+chown -R root "${1}"
+chgrp -R wheel "${1}"
+setfacl -R -m group@:full_set:fd:allow "${1}"
+setfacl -R -m owner@:full_set:fd:allow "${1}"
+setfacl -R -m everyone@:full_set:fd:allow "${1}"
 
