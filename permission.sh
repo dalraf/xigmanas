badd04770ac2fdd6297183f6b8e696ed8e177a3f
Daniel Fonseca Alves <dalraf@gmail.com>
2024-03-15 10:26:54 -0300

    Correção

--- permission.sh
+++ permission.sh
@@ -2,7 +2,8 @@
 setfacl -R -b "${1}"
 chown -R root "${1}"
 chgrp -R wheel "${1}"
-setfacl -R -m group@:full_set:fd:allow "${1}"
-setfacl -R -m owner@:full_set:fd:allow "${1}"
-setfacl -R -m everyone@:full_set:fd:allow "${1}"
+setfacl -R -m "group@:full_set:fd:allow" "${1}"
+setfacl -R -m "owner@:full_set:fd:allow" "${1}"
+setfacl -R -m "everyone@:full_set:fd:allow" "${1}"
+setfacl -R -m "group:usuários_do_domínio:rwxpDdaARWcCos:fd-----:allow" "${1}"
 
